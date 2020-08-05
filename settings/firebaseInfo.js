ed7c44ad56890734ebfd74022162ff250a879f70
saralinjawi <69105387+saralinjawi@users.noreply.github.com>
2020-08-05 16:24:59 +0300

    Add files via upload

---
+++ settings/firebaseInfo.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAJgBy9rgv3NfwQkZ57nLBengmc7bGD3WU",
+    authDomain: "gamification-trial1.firebaseapp.com",
+    databaseURL: "https://gamification-trial1.firebaseio.com",
+    projectId: "gamification-trial1",
+    storageBucket: "gamification-trial1.appspot.com",
+    messagingSenderId: "509381495507",
+    appId: "1:509381495507:web:e1dc5519a17ea5e5dc6226",
+    measurementId: "G-VDBZNVRCH8"
+};
+firebase.initializeApp(config);
+var rootRef = firebase.database().ref();
+
